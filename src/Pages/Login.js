feba9444356ff2b83baf55b27cfa2e8b92c1eb2b
Jovani Matthieu <ravelonarivojovani@gmail.com>
2024-07-04 22:55:49 +0300

    Add card2

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,6 +1,12 @@
 import {Component} from 'react';
 
 export default class Login extends Component{
+    state = {
+        player : [
+            username = '',
+            passwoard = ''
+        ]
+    }
     render(){
         return(
             <div className='login'>
--- src/Pages/Platform.js
+++ src/Pages/Platform.js
@@ -1,6 +1,15 @@
 import { Component } from 'react';
 
 export default class Platform extends Component{
+    state = {
+        platform : [
+            players = [],
+            gameCardColor = "",
+            gameCardName = "",
+            gameCardValue = Null
+        ]
+    }
+    
     render(){
         return(
             <div className="platform">
